5288a8799ecef95f3a6391a6e8c6e4dee2aff1ce
maxim <maximkadocnikov@gmail.com>
2024-05-02 11:15:43 +0700

    fix link navigation

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import Link from 'next/link'
-import {useRouter} from 'next/router'
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-const LinkComponent = ({children, skipLocaleHandling, ...rest}: any) => {
-    const router = useRouter()
-    const locale = rest.locale || router.query.locale || ''
+const LinkComponent = ({ children, skipLocaleHandling, ...rest }: any) => {
+  const router = useRouter();
+  const locale = rest.locale || router.query.locale || '';
 
-    let href = rest.href || router.asPath
-    if (href.indexOf('http') === 0) skipLocaleHandling = true
-    if (locale && !skipLocaleHandling) {
-        href = href
-            ? `/${locale}${href}`
-            : router.pathname.replace('[locale]', locale)
-    }
+  let href = rest.href || router.asPath;
+  if (href.indexOf('http') === 0) skipLocaleHandling = true;
+  if (locale && !skipLocaleHandling) {
+    href = href
+      ? `/${locale}${href}`
+      : router.pathname.replace('[locale]', locale);
+  }
 
-    return (
-        <>
-            <Link href={href} {...rest}>
-                {children}
-            </Link>
-        </>
-    )
-}
+  return (
+    <>
+      <Link href={href}>
+        {children}
+      </Link>
+    </>
+  );
+};
 
-export default LinkComponent
+export default LinkComponent;
